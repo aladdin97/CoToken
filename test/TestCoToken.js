f6754724464847c6480a052d9fa76fb89557d86e
Mike <amikea97@gmail.com>
2020-03-07 21:34:49 +0200

    added one more test - Co not able to burn tokens in others' balances

--- test/TestCoToken.js
+++ test/TestCoToken.js
@@ -29,8 +29,6 @@ contract('CoToken', function (accounts) {
 
         var ethBalance = weiBalance/1e18
         var ethPrice = weiPrice/1e18
-        
-        // console.log(typeof weiPrice )
 
         assert.equal(balance.toNumber(), 1, "Incorrect number of tokens were minted to address")
         assert.equal(totalSupply.toNumber(),1,"totalSupply was not updated")
@@ -54,7 +52,7 @@ contract('CoToken', function (accounts) {
 
       // TESTING OF BURN FUNCTION
       it('should be able to burn tokens and update respective CoToken and Eth balances', async function () {
-        // minter is a public variable in the contract so you can get it directly via the created call function
+
         let weiPrice = await CoTokenInstance.buyPrice(5)
         await CoTokenInstance.mint(accounts[0],5,{"from": accounts[0],"value":weiPrice})
         let weiCo_PrevBalance = await CoTokenInstance.weiBalance(accounts[0])
@@ -77,12 +75,13 @@ contract('CoToken', function (accounts) {
         assert.equal(balance.toNumber(), 0, "Balance after burn is incorrect")
         assert.equal(totalSupply.toNumber(),0,"totalSupply after burn is incorrect")
         assert.equal(ethBalance,0,"Eth contract balance was not correctly updated")
+
         assert.equal(ethCoBalance.toFixed(3),ethSellPrice,"Co's Eth balance was not correctly updated")
-        // round off to 3 decimals to account for gas costs
+        // toFixed(3) is used to round off to 3 decimals to account for gas costs
       }) 
       
       it('should not be able to burn more tokens than the current total supply', async function () {
-        // minter is a public variable in the contract so you can get it directly via the created call function
+
         let weiPrice = await CoTokenInstance.buyPrice(5)
         await CoTokenInstance.mint(accounts[0],5,{"from": accounts[0],"value":weiPrice})
 
@@ -92,7 +91,7 @@ contract('CoToken', function (accounts) {
       })  
 
       it('should only burn if the owner (Co) calls the burn function', async function () {
-        // minter is a public variable in the contract so you can get it directly via the created call function
+
         let weiPrice = await CoTokenInstance.buyPrice(5)
         await CoTokenInstance.mint(accounts[1],5,{"from": accounts[1],"value":weiPrice})
 
@@ -101,12 +100,22 @@ contract('CoToken', function (accounts) {
         
       })
 
+      it('should not burn other peoples tokens ie Co cannot burn tokens not in his balance', async function () {
+
+        let weiPrice = await CoTokenInstance.buyPrice(5)
+        await CoTokenInstance.mint(accounts[1],5,{"from": accounts[1],"value":weiPrice})
+
+
+        await truffleAssert.reverts(CoTokenInstance.burn(accounts[1], 5,{"from": accounts[0]}))
+        
+      })
+
       // END OF BURN FUNCTION TESTING 
       
 
       // TESTING OF DESTROY FUNCTION 
       it('should only destroy contract if the owner (Co) owns all 100 tokens', async function () {
-        // minter is a public variable in the contract so you can get it directly via the created call function
+
         let weiPrice = await CoTokenInstance.buyPrice(5)
         await CoTokenInstance.mint(accounts[0],5,{"from": accounts[0],"value":weiPrice})
 
@@ -116,7 +125,7 @@ contract('CoToken', function (accounts) {
       })
 
       it('should only destroy contract if the owner (Co) calls the destroy function', async function () {
-        // minter is a public variable in the contract so you can get it directly via the created call function
+
         let weiPrice = await CoTokenInstance.buyPrice(100)
         await CoTokenInstance.mint(accounts[1],100,{"from": accounts[1],"value":weiPrice})
 
