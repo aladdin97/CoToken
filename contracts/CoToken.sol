f19b697dcde02955a8a3095c6a5355503cfd7434
Mike <amikea97@gmail.com>
2020-03-07 21:33:44 +0200

    Added functionality to prevent Co from burning tokens not in his own balance

--- contracts/CoToken.sol
+++ contracts/CoToken.sol
@@ -1,8 +1,7 @@
 pragma solidity >=0.5.1;
-// import "./Ownable.sol";
-// import "./SafeMath.sol";
-// import "./IERC20.sol";
-// import "./ERC20.sol";
+
+// not requires open-zeppelin installation 
+// npm install open-zeppelin was used 
 import "openzeppelin-solidity/contracts/token/ERC20/ERC20.sol";
 import "openzeppelin-solidity/contracts/ownership/Ownable.sol";
 
@@ -48,8 +47,8 @@ using SafeMath for uint256;
         uint wei_2_return = sellPrice(amount);
     
 
-        // // Prevent co from burning other peoples tokens ie he should only be able to burn tokens in his balance
-        // require(account == msg.sender,"Co cannot burn tokens that are not in his account balance");
+        // Prevent co from burning other peoples tokens ie he should only be able to burn tokens in his balance or in the contract itself
+        require(account == msg.sender || account == address(this),"Co cannot burn tokens that are not in his account balance or in the contract");
 
 
         _burn(account,amount);
@@ -57,15 +56,19 @@ using SafeMath for uint256;
         //transfer eth back to owner
         owner.transfer(wei_2_return);
 
-        // uint256 price = sellPrice(amount);
 
 
     }
 
     function destroy() external  onlyOwner(){
+
+        // ensure that owner must be in possession of all 100 tokens in order to destroy the contract
         require(balanceOf(msg.sender) == 100, "Sender does not own all tokens");
         selfdestruct(msg.sender);
     }
+
+    // function to return eth balances to ensure correct amount of eth is transferred 
+    // to the respective parties whilst using the bonding curve 
 
     function weiBalance(address payable _address) public view returns (uint){
         return _address.balance;
